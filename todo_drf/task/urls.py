5f17c0b9f0a3ea99f069e7f05672829071fe5529
Saurabh Kumar Misrha <saurabh.200218@gmail.com>
2024-01-10 23:05:41 +0530

    updating views
    changing from function to class based views

--- todo_drf/task/urls.py
+++ todo_drf/task/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.taskOverview, name="task-overview"),
-    path("auth/", views.AuthorizationView, name="AuthorizationView"),
-    path("task-list/", views.taskList, name="task-list"),
-    path("task-detail/<str:pk>/", views.taskDetail, name="task-detail"),
-    path("task-update/<str:pk>/", views.taskUpdate, name="task-update"),
-    path("task-create/", views.taskCreate, name="task-create"),
-    path("task-delete/<str:pk>/", views.taskDelete, name="task-delete"),
-    path("tag-list/", views.tagList, name="tag-list"),
+    path("", views.TaskOverview.as_view(), name="task-overview"),
+    path("auth/", views.AuthorizationView.as_view(), name="AuthorizationView"),
+    path("task-list/", views.TaskList.as_view(), name="task-list"),
+    path("task-detail/<str:pk>/", views.TaskDetail.as_view(), name="task-detail"),
+    path("task-update/<str:pk>/", views.TaskUpdate.as_view(), name="task-update"),
+    path("task-create/", views.TaskCreate.as_view(), name="task-create"),
+    path("task-delete/<str:pk>/", views.TaskDelete.as_view(), name="task-delete"),
+    path("tag-list/", views.TagList.as_view(), name="tag-list"),
 ]
--- todo_drf/task/views.py
+++ todo_drf/task/views.py
@@ -1,110 +1,114 @@
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.decorators import permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from .serializers import TaskSerializer, TagSerializer
 from .models import Task, Tag
 
-# Create your views here.
+
+class AuthorizationView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        msg = {
+            "message": f"Hi {request.user.username}! Congratulations on being authenticated!"
+        }
+        return Response(msg, status=status.HTTP_200_OK)
 
 
-@api_view(["GET"])
-@permission_classes((IsAuthenticated,))
-def AuthorizationView(request):
-    msg = {
-        "message": f"Hi {request.user.username}! Congratulations on being authenticated!"
-    }
-    return Response(msg, status=status.HTTP_200_OK)
+class TaskOverview(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        task_urls = {
+            "List": "/task-list/",
+            "Detail-View": "/task-detail/<str:pk>/",
+            "Create": "/task-create/",
+            "Delete": "/task-delete/<str:pk>/",
+            "Update": "/task-update/<str:pk>/",
+        }
+        return Response(task_urls)
 
 
-@api_view(["GET"])
-@permission_classes((IsAuthenticated,))
-def taskOverview(request):
-    task_urls = {
-        "List": "/task-list/",
-        "Detail-View": "/task-detail/<str:pk>/",
-        "Create": "/task-create/",
-        "Delete": "/task-delete/<str:pk>/",
-        "Update": "/task-update/<str:pk>/",
-    }
-    return Response(task_urls)
+class TaskList(APIView):
+    permission_classes = [IsAuthenticated]
 
-
-@api_view(["GET"])
-@permission_classes((IsAuthenticated,))
-def taskList(request):
-    tasks = Task.objects.all()
-    serializer = TaskSerializer(tasks, many=True)
-    return Response(serializer.data)
-
-
-@api_view(["GET"])
-@permission_classes((IsAuthenticated,))
-def taskDetail(request, pk):
-    tasks = Task.objects.get(id=pk)
-    serializer = TaskSerializer(tasks, many=False)
-    return Response(serializer.data)
-
-
-@api_view(["POST"])
-@permission_classes((IsAuthenticated,))
-def taskUpdate(request, pk):
-    task = Task.objects.get(id=pk)
-    serializer = TaskSerializer(instance=task, data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data)
-
-
-@api_view(["POST"])
-@permission_classes((IsAuthenticated,))
-def taskCreate(request):
-    # Extract data from the request
-    title = request.data.get("title")
-    description = request.data.get("description")
-    due_date = request.data.get("Due_date")
-    status = request.data.get("status")
-    tag_titles = request.data.get("tag", [])
-
-    # Check if the tags exist, create them if not present
-    tags = []
-    for tag_title in tag_titles:
-        tag, created = Tag.objects.get_or_create(
-            title=tag_title, defaults={"id": tag_title}
-        )
-        tags.append(tag)
-
-    # Create a new task with the extracted data and tags
-    task_data = {
-        "title": title,
-        "description": description,
-        "Due_date": due_date,
-        "status": status,
-    }
-
-    # Adding tags to the task data
-    task_data["tag"] = tags
-
-    serializer = TaskSerializer(data=task_data)
-    if serializer.is_valid():
-        serializer.save()
+    def get(self, request):
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
         return Response(serializer.data)
-    else:
-        return Response("Data is invalid")
 
 
-@api_view(["DELETE"])
-@permission_classes((IsAuthenticated,))
-def taskDelete(request, pk):
-    task = Task.objects.get(id=pk)
-    task.delete()
-    return Response("Task deleted successfully.")
+class TaskDetail(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, pk):
+        tasks = Task.objects.get(id=pk)
+        serializer = TaskSerializer(tasks, many=False)
+        return Response(serializer.data)
 
 
-@api_view(["GET"])
-@permission_classes((IsAuthenticated,))
-def tagList(request):
-    tags = Tag.objects.all()
-    serializer = TagSerializer(tags, many=True)
-    return Response(serializer.data)
+class TaskUpdate(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, pk):
+        task = Task.objects.get(id=pk)
+        serializer = TaskSerializer(instance=task, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data)
+
+
+class TaskCreate(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        title = request.data.get("title")
+        description = request.data.get("description")
+        due_date = request.data.get("Due_date")
+        status = request.data.get("status")
+        tag_titles = request.data.get("tag", [])
+
+        # Check if the tags exist, create them if not present
+        tags = []
+        for tag_title in tag_titles:
+            tag, created = Tag.objects.get_or_create(
+                title=tag_title, defaults={"id": tag_title}
+            )
+            tags.append(tag)
+
+        # Create a new task with the extracted data and tags
+        task_data = {
+            "title": title,
+            "description": description,
+            "Due_date": due_date,
+            "status": status,
+        }
+
+        # Adding tags to the task data
+        task_data["tag"] = tags
+
+        serializer = TaskSerializer(data=task_data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response("Data is invalid")
+
+
+class TaskDelete(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def delete(self, request, pk):
+        task = Task.objects.get(id=pk)
+        task.delete()
+        return Response("Task deleted successfully.")
+
+
+class TagList(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        tags = Tag.objects.all()
+        serializer = TagSerializer(tags, many=True)
+        return Response(serializer.data)
